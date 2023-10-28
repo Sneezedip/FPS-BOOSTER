22aa8500645adb46d89da3fff8144998f62fba8d
Afonso Cabral <afonsocabral11@gmail.com>
2023-10-28 00:49:01 +0000

    Added an option to switch light mode and night

--- fpsbooster.py
+++ fpsbooster.py
@@ -1,4 +1,5 @@
 import ctypes
+import time
 import customtkinter
 from PIL import Image
 import tkinter.messagebox as mbox
@@ -8,7 +9,8 @@ icon = "images/icon.ico"
 logo = customtkinter.CTkImage(Image.open("images\Logo.png"), size=(100, 100))
 appdata = os.getenv('LOCALAPPDATA')
 radius = 20
-
+canSwitch = True
+i = 0
 def windowConfig(window,x,y):
     window.eval('tk::PlaceWindow . center')
     window.maxsize(x,y)
@@ -32,16 +34,51 @@ def tempWindow():
     displaylogo = customtkinter.CTkLabel(tempw,height=200,width=200,image=logo,text="")
     displaylogo.place(x=50,y=5)
 
-    progressbar = customtkinter.CTkProgressBar(tempw, orientation="horizontal",mode="indeterminate",indeterminate_speed=2,corner_radius=3)
+    progressbar = customtkinter.CTkProgressBar(tempw, orientation="horizontal",mode="indeterminate",indeterminate_speed=4,corner_radius=3)
     progressbar.place(x=50,y=200)
     progressbar.start()
     
     set_window_shape(tempw, radius)
 
-    tempw.after(2500,UpdateW)
+    tempw.after(1250,UpdateW)
     tempw.mainloop()
 
+def SwitchMode():
+    global canSwitch,i
+    currentSelection = bgswitch.get()
+    def NoSpam():
+        border.configure(bg_color=color_code)
+        cleanMemoryButton.configure(bg_color=color_code)
+        netOptimizeButton.configure(bg_color=color_code)
+        fortniteLow.configure(bg_color=color_code)
+        fortniteLowLabel.configure(bg_color=color_code)
+    if canSwitch:
+        canSwitch = False
+        if i != 85:
+            for i in range(260,80,-5):
+                color_code = "#{:02X}{:02X}{:02X}".format(i, i, i)
+                NoSpam()
+                root.update()
+                time.sleep(0.01)
+            if i == 85: 
+                NoSpam()
+                root.update()
+                canSwitch = True
+        else:
+            for i in range(80,260,5):
+                    color_code = "#{:02X}{:02X}{:02X}".format(i, i, i)
+                    NoSpam()
+                    root.update()
+                    time.sleep(0.01)
+            if i == 255: 
+                NoSpam()
+                root.update()
+                canSwitch = True
+    else:
+        bgswitch.select(currentSelection)
+
 def Main(tempw):
+    global root,bgswitch
     tempw.destroy()
     root = customtkinter.CTk()
     windowConfig(root,400,300)
@@ -50,6 +87,7 @@ def Main(tempw):
     root.mainloop()
 
 def LoadLabels(root):
+    global bgswitch,border,cleanMemoryButton,netOptimizeButton,fortniteLow,fortniteLowLabel 
     def start_drag(event):
             global x,y
             x = event.x
@@ -58,25 +96,27 @@ def LoadLabels(root):
         new_x = (root.winfo_x() + event.x) - x
         new_y = (root.winfo_y() + event.y) - y
         root.geometry(f"+{new_x}+{new_y}")
-
-    border = customtkinter.CTkFrame(root,400,300,border_color="lightblue",border_width=4,bg_color="transparent",fg_color="transparent")
+    
+    border = customtkinter.CTkFrame(root,400,300,border_color="lightblue",border_width=4,bg_color="white",fg_color="transparent")
 
     title_bar = customtkinter.CTkFrame(root,height=25,width=800,bg_color="lightblue",fg_color="lightblue")
+    titleLabel = customtkinter.CTkLabel(root,text="FPS BOOSTER",font=("Arial Baltic",17),height=25,text_color="black",bg_color="lightblue",fg_color="transparent")
     title_bar.bind("<ButtonPress-1>",start_drag)
     title_bar.bind("<B1-Motion>",move_window)
-    titleLabel = customtkinter.CTkLabel(root,text="FPS BOOSTER",font=("Arial Baltic",17),height=25,text_color="black",bg_color="lightblue",fg_color="transparent")
     titleLabel.bind("<ButtonPress-1>",start_drag)
     titleLabel.bind("<B1-Motion>",move_window)
 
+    bgswitch = customtkinter.CTkSwitch(root,corner_radius=10,text="",bg_color="lightblue",button_color="grey",fg_color="white",progress_color="black",command=SwitchMode)
     off = customtkinter.CTkImage(Image.open("images\off.png"), size=(15, 15))
     close_button = customtkinter.CTkButton(root,text="",image=off,bg_color="lightblue",fg_color="lightblue",hover_color="lightblue",width=10,height=10,command=root.destroy,font=("verdana bold",15))
-    cleanMemoryButton = customtkinter.CTkButton(root,text = "Optimize Windows",font=("verdana",15),text_color="black",fg_color="lightblue",hover_color="white",command=Optimization)
-    netOptimizeButton = customtkinter.CTkButton(root,text = "Optimize Internet  ",font=("verdana",15),text_color="black",fg_color="lightblue",hover_color="white",command=NetOptimization)
-    fortniteLow = customtkinter.CTkButton(root,text = "Optimize Fortnite",font=("verdana",15),text_color="black",fg_color="lightblue",hover_color="white",command=FortniteLowGraphics)
+    cleanMemoryButton = customtkinter.CTkButton(root,text = "Optimize Windows",font=("verdana",15),text_color="black",fg_color="lightblue",hover_color="white",corner_radius=9,command=Optimization)
+    netOptimizeButton = customtkinter.CTkButton(root,text = "Optimize Internet  ",font=("verdana",15),text_color="black",fg_color="lightblue",hover_color="white",corner_radius=9,command=NetOptimization)
+    fortniteLow = customtkinter.CTkButton(root,text = "Optimize Fortnite",font=("verdana",15),text_color="black",fg_color="lightblue",bg_color="transparent",hover_color=None,corner_radius=9,command=nightMode)
     fortniteLowLabel = customtkinter.CTkLabel(root,text="(low graphics)",font=("verdana",12),text_color="grey",bg_color="transparent",fg_color="transparent")
-
+  
     border.place(x=0,y=0)
     title_bar.place(x=0,y=0)
+    bgswitch.place(x=10,y=0)
     close_button.place(x=370,y=0)
     titleLabel.place(x=150,y=0)
     cleanMemoryButton.place(x=30,y=70)
@@ -84,7 +124,6 @@ def LoadLabels(root):
     fortniteLowLabel.place(x=255,y=40)
     fortniteLow.place(x=230,y=70)
 
-       
 def Optimization():
     autorization = mbox.askquestion(title="Confirmation",message="This will run some commands in your command line, do you want to continue?")
     if autorization == "yes":
@@ -128,5 +167,4 @@ def FortniteLowGraphics():
         mbox.showinfo(title="FPS BOOSTER",message="Operation canceled.")
         return
     
-
 tempWindow()
